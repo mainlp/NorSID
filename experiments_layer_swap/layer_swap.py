955710e562cc412b7713e8a6f2504f00dcb9f68e
Felicia Körner <fkoerner@cis.uni-muenchen.de>
2024-11-15 09:53:17 +0100

    modify layer swap to allow for local or remote

--- experiments_layer_swap/layer_swap.py
+++ experiments_layer_swap/layer_swap.py
@@ -6,10 +6,15 @@ from transformers import DebertaV2ForMaskedLM
 sys.path.insert(0, '../../JointBERT')
 
 def swap_layers(layers_to_swap):
-    lang_expert = DebertaV2ForMaskedLM.from_pretrained("lang_expert/ndc/models/checkpoint-0")
-    slot_intent_expert = torch.load("slot_intent_expert/norsid/model_7", map_location=torch.device('cpu'))
-    w_task = 1
-    w_lang = 0
+    local = False
+    device = "cpu" if local else "cuda:0"
+    lang_dir = "lang_expert/ndc/models" if local else "/nfs/gdata/fkoerner/ndc0.5_lang_exps"
+    slot_intent_dir = "slot_intent_expert/models/norsid" if local else "/nfs/gdata/fkoerner/norsid_mdeberta_model"
+    lang_expert = DebertaV2ForMaskedLM.from_pretrained(f"{lang_dir}/checkpoint-2178")
+    lang_expert.to(device)
+    slot_intent_expert = torch.load(f"{slot_intent_dir}/model_7", map_location=device)
+    w_task = 0.5
+    w_lang = 0.5
     state_dict = {}
     for index, ((name, lang_param), (name, slot_intent_param)) in enumerate(zip(lang_expert.deberta.named_parameters(),
                                                                               slot_intent_expert.deberta.named_parameters())):
@@ -27,13 +32,14 @@ def swap_layers(layers_to_swap):
             # merge the parameters
             state_dict[name] = (w_task * slot_intent_param + w_lang * lang_param) / (w_task + w_lang)
     slot_intent_expert.deberta.load_state_dict(state_dict)
-    torch.save(slot_intent_expert, f"merged_model_7_{''.join([str(l) for l in layers_to_swap])}_w{w_task}_{w_lang}")
+    torch.save(slot_intent_expert, f"/nfs/gdata/fkoerner/swapped_models/merged_model_7_{''.join([str(l) for l in layers_to_swap])}_w{w_task}_{w_lang}")
 
 
 def main():
-    layers = [i for i in range(12)]
-    combinations_layers_to_swap = combinations(layers, 3)
-    contiguous = [l for l in combinations_layers_to_swap if l[1] == (l[0] + 1) and l[2] == (l[1] + 1)]
+    layers = [i for i in range(6)]
+    combinations_layers_to_swap = combinations(layers, 2)
+    contiguous = [l for l in combinations_layers_to_swap if l[1] == (l[0] + 1)]
+    contiguous = [[0, 1]]
     for layers_to_swap in contiguous:
         swap_layers(layers_to_swap)
 
