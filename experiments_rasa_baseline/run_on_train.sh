b5ab75aa82e9cd600f1850eb26614f62cac9747c
fkoerner <koernerfelicia@gmail.com>
2024-12-12 16:05:44 +0100

    remove code related to unused rasa baseline

--- experiments_rasa_baseline/run_on_train.sh
+++
@@ -1,6 +0,0 @@
-cd nor || exit
-rasa test nlu --nlu data/nlu.yml --out results-train
-cd ../en || exit
-rasa test nlu --nlu data/nlu.yml --out results-train
-cd ../bert-multiling-en || exit
-rasa test nlu --nlu ../en/data/nlu.yml --out results-train
--- experiments_rasa_baseline/run_tests.sh
+++
@@ -1,6 +0,0 @@
-cd nor || exit
-rasa test nlu --nlu ./tests/test_nlu.yml --out results-nor
-cd ../en || exit
-rasa test nlu --nlu ../nor/tests/test_nlu.yml --out results-0-shot
-cd ../bert-multiling-en || exit
-rasa test nlu --nlu ../nor/tests/test_nlu.yml --out results-0-shot
