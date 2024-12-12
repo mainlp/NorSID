ea79b8f48d03900515f1b637b420a4cb9df80530
fkoerner <koernerfelicia@gmail.com>
2024-12-12 17:57:34 +0100

    cleanup visualization code

---
+++ experiments_layer_swap/util.py
@@ -0,0 +1,13 @@
+import torch
+from transformers import DebertaV2ForMaskedLM
+
+def load_model(model_location, no_cuda=False):
+    try:
+        model = DebertaV2ForMaskedLM.from_pretrained(model_location)
+    except (EnvironmentError, KeyError):
+        model = torch.load(model_location, map_location=get_device(no_cuda))
+    model.eval()
+    return model
+
+def get_device(no_cuda=False):
+    return torch.device("cuda" if torch.cuda.is_available() and not no_cuda else "cpu")
